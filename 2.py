02558ecf15d9056b1a7588c59f230c9a50acf448
Alex Shi <yoip1117@gmail.com>
2015-03-27 01:38:12 -0400

    Update 2.py

--- 2.py
+++ 2.py
@@ -6,3 +6,5 @@ def two():
 	return c-1
 
 sum([n for n in [int(((.5+5**.5/2)**n-(-.5-5**.5/2)**-n)/5**.5) for n in range(34)] if n%2-1])
+
+r=.5+5**.5/2;sum([n for n in[int((r**n-(-r)**-n)/5**.5) for n in range(34)] if n%2-1])
