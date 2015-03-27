2427c3d9849efae5933a1c2598a3012beece942b
Alex Shi <yoip1117@gmail.com>
2015-03-27 01:50:18 -0400

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,2 +1,1 @@
-from itertools import combinations
-max([i[0]*i[1]for i in combinations(range(100,1000),2)if str(i[0]*i[1])==str(i[0]*i[1])[::-1]])
+import itertools;max([i[0]*i[1]for i in itertools.combinations(range(100,1000),2)if str(i[0]*i[1])==str(i[0]*i[1])[::-1]])
