12439bbb1c7655aecf1081b375c742567b962ab2
Alex Shi <yoip1117@gmail.com>
2015-03-27 02:03:15 -0400

    Update 2.py

--- 2.py
+++ 2.py
@@ -5,6 +5,6 @@ def two():
 		if b%2-1:c+=b
 	return c-1
 
-sum([n for n in[int(((.5+5**.5/2)**n-(-.5-5**.5/2)**-n)/5**.5)for n in range(34)]if n%2-1])
+sum(n for n in[int(((.5+5**.5/2)**n-(-.5-5**.5/2)**-n)/5**.5)for n in range(34)]if n%2-1)
 
-r=.5+5**.5/2;sum([n for n in[int((r**n-(-r)**-n)/5**.5)for n in range(34)]if n%2-1])
+r=.5+5**.5/2;sum(n for n in[int((r**n-(-r)**-n)/5**.5)for n in range(34)]if n%2-1)
