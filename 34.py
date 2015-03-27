7e1f7072a936de4a6ae1c20ac4874a810e2f554d
Alex Shi <yoip1117@gmail.com>
2015-03-27 01:59:25 -0400

    Update 34.py

--- 34.py
+++ 34.py
@@ -1,2 +1,1 @@
-import math
-sum([i for i in range(3,1000000) if sum([math.factorial(int(x)) for x in str(i)])==i])
+import math;sum(i for i in range(3,1000000)if sum(math.factorial(int(x))for x in str(i))==i)
